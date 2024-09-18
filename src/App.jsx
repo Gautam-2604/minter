d7b4b5dcc74c2527558d2e54a825064abaad3c97
Gautam-2604 <gaurisariag@gmail.com>
2024-09-19 02:47:01 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
-// Default styles that can be overridden by your app
+
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { TokenLaunchpad } from './components/TokenLaunchpad';
 
